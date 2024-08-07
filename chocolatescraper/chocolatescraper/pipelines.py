a51dd6ff32342d3aa8cea8b397b2262056ddb926
pidokige02 <pidokige0204@gmail.com>
2024-08-07 19:39:50 +0900

    adding customed pipelines

--- chocolatescraper/chocolatescraper/pipelines.py
+++ chocolatescraper/chocolatescraper/pipelines.py
@@ -6,8 +6,41 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from scrapy.exceptions import DropItem
 
 
 class ChocolatescraperPipeline:
     def process_item(self, item, spider):
         return item
+
+class PriceToUSDPipeline:
+    gbpToUsdRRate = 1.3
+
+    def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+
+        if adapter.get('price'):
+
+            float_price = float(adapter['price'])
+
+            adapter['price'] = float_price * self.gbpToUsdRRate
+
+            return item
+
+        else:
+            raise DropItem("Missing price in {item}")
+
+
+class DuplicatePipeline:
+
+    def __init__(self):
+        self.name_seen = set()
+
+    def process_item(self, item, spider):
+        adapter = ItemAdapter(item)
+
+        if adapter.get('name') in self.name_seen:
+            raise DropItem("Duplicate item found : {item!r}")
+        else:
+            self.name_seen.add(adapter['name'])
+            return item
