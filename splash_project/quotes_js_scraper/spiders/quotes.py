8a9e31cdb77704de177c4f749c8111411aff2483
pidokige02 <pidokige0204@gmail.com>
2024-08-09 20:20:07 +0900

    Running JavaScript Scripts

--- splash_project/quotes_js_scraper/spiders/quotes.py
+++ splash_project/quotes_js_scraper/spiders/quotes.py
@@ -3,6 +3,9 @@ import base64
 from quotes_js_scraper.items import QuoteItem
 from scrapy_splash import SplashRequest
 
+javascript_script = """
+element = document.querySelector('h1').innerHTML = 'The best quotes of all time!'
+"""
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
 
@@ -13,11 +16,13 @@ class QuotesSpider(scrapy.Spider):
             callback=self.parse,
             endpoint='render.json',
             args={
+                'wait': 2,
+                'js_source': javascript_script,
                 'html': 1,
                 'png': 1,
                 'width': 1000,
             })
-
+        
     def parse(self, response):
         imgdata = base64.b64decode(response.data['png'])
         filename = 'some_image.png'
