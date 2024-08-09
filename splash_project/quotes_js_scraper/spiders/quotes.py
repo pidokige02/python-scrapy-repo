dc591afcc866e44f54700269f3707f1ea1e45adc
pidokige02 <pidokige0204@gmail.com>
2024-08-09 19:13:38 +0900

    Scrolling The Page

--- splash_project/quotes_js_scraper/spiders/quotes.py
+++ splash_project/quotes_js_scraper/spiders/quotes.py
@@ -3,14 +3,22 @@ from quotes_js_scraper.items import QuoteItem
 from scrapy_splash import SplashRequest
 
 lua_script = """
-function main(splash, args)
-    assert(splash:go(args.url))
+function main(splash)
+    local num_scrolls = 10
+    local scroll_delay = 1.0
 
-  while not splash:select('div.quote') do
-    splash:wait(0.1)
-    print('waiting...')
-  end
-  return {html=splash:html()}
+    local scroll_to = splash:jsfunc("window.scrollTo")
+    local get_body_height = splash:jsfunc(
+        "function() {return document.body.scrollHeight;}"
+    )
+    assert(splash:go(splash.args.url))
+    splash:wait(splash.args.wait)
+
+    for _ = 1, num_scrolls do
+        scroll_to(0, get_body_height())
+        splash:wait(scroll_delay)
+    end
+    return splash:html()
 end
 """
 
@@ -23,7 +31,7 @@ class QuotesSpider(scrapy.Spider):
             url,
             callback=self.parse,
             endpoint='execute',
-            args={'wait': 0.5, 'lua_source': lua_script,  url :'https://quotes.toscrape.com/scroll'}
+            args={'wait': 2, 'lua_source': lua_script}
             )
 
     def parse(self, response):
