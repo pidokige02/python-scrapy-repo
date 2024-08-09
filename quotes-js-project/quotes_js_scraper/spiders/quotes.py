43ccb9ff7924db333bb88ac14b2ef35890474af0
pidokige02 <pidokige0204@gmail.com>
2024-08-09 11:02:36 +0900

    Wait for the page to finish loading specific page element CSS selectors

--- quotes-js-project/quotes_js_scraper/spiders/quotes.py
+++ quotes-js-project/quotes_js_scraper/spiders/quotes.py
@@ -1,6 +1,6 @@
 import scrapy
 from quotes_js_scraper.items import QuoteItem
-
+from scrapy_playwright.page import PageMethod
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
@@ -9,8 +9,13 @@ class QuotesSpider(scrapy.Spider):
 
 
     def start_requests(self):
-        url = "https://quotes.toscrape.com/js/"
-        yield scrapy.Request(url, meta={'playwright': True})
+        url = 'https://quotes.toscrape.com/js/'
+        yield scrapy.Request(url, meta=dict(
+			playwright = True,
+			playwright_include_page = True,
+            playwright_page_methods =[PageMethod('wait_for_selector', 'div.quote')],
+                    errback=self.errback,
+		))
 
     def parse(self, response):
         for quote in response.css('div.quote'):
@@ -19,3 +24,7 @@ class QuotesSpider(scrapy.Spider):
             quote_item['author'] = quote.css('small.author::text').get()
             quote_item['tags'] = quote.css('div.tags a.tag::text').getall()
             yield quote_item
+
+    async def errback(self, failure):
+        page = failure.request.meta["playwright_page"]
+        await page.close()
